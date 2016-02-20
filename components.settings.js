ccb2cdc790c0a899aac9bc726ba5714f9ac0811a
Van <i_van@list.ru>
2016-02-20 10:59:49 +0300

    added default components path settings

---
+++ components.settings.js
@@ -0,0 +1,11 @@
+define(function () {
+
+  return {
+    formats: {
+      template: '{app}/{components}/{template}/{template}.template',
+      vm: '{app}/{components}/{vm}/{vm}.vm',
+      factory: '{app}/{components}/{factory}/{factory}.factory'
+    }
+  };
+
+});
