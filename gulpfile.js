c68520fbb6671bac8de5576c59c53184dc1c2004
VanDalkvist <i_van@list.ru>
2016-03-03 23:05:57 +0300

    added r.js (requirejs) gulp build;

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+// dependencies
+
+var gulp = require('gulp');
+var requirejsOptimize = require('gulp-requirejs-optimize');
+var path = require('path');
+var _ = require('lodash');
+var fs = require('fs');
+
+// preparation
+
+var paths = {
+    config: './tools/build.config.json',
+    src: './src/**/*.js',
+    dist: 'dist',
+    outFileName: 'component-loader.js',
+    outMinFileName: 'component-loader.min.js'
+};
+
+// gulp tasks
+
+gulp.task('default', ['build']);
+
+gulp.task('build', function () {
+    return _build({
+        optimize: 'none',
+        out: paths.outFileName
+    });
+});
+
+gulp.task('minify', function () {
+    return _build({
+        out: paths.outMinFileName
+    });
+});
+
+function _build(options) {
+    var content = fs.readFileSync(paths.config);
+    var config = JSON.parse(content);
+
+    options = _.extend(config, options);
+
+    return gulp.src(paths.src)
+        .pipe(requirejsOptimize(options))
+        .pipe(gulp.dest(paths.dist));
+}
+
+/*
+
+ "wrap": {
+ "startFile": "wrap.start",
+ "endFile": "wrap.end"
+ }
+ */
